bcf70c56e31a6c5f226455a91e1ae3ff22938add
brleinad <daniel.rodas.bautista@gmail.com>
2016-01-29 19:12:12 +0000

    generic nonlinear CLE decomposed part now works!

--- CLA_decomposed/source/gen_nonlinear_part.v
+++ CLA_decomposed/source/gen_nonlinear_part.v
@@ -8,66 +8,34 @@ input c; // carry in
 
 output [NNL-1:0] n; // non-linear outputs
 
-reg [NNL:0] g = 0; //non-linear intermediate values
+reg [NNL:0] g ;//= 0; //non-linear intermediate values
+//reg [NNL-1:0] n ;
 integer i,j, ii,inter;
 
 always @(a or b or c)
 begin
         //initial values
-        i = 1; 
-        j = 0; //index for g and index for a, b
+        i = 1; //index for g
+        j = 0; //index for a, b
         inter = 1; //interval
 
-        g[0] = c;
+        g[0] = c; 
 
         while (i < NNL)
         begin 
                 g[i] = a[j] & b[j];
-                i = i + i;  //go to next g
-                ii = 0;
-                while (ii < inter)
+                i = i + 1;  //go to next g
+                for (ii = 0; ii < inter; ii = ii + 1)
                 begin
-                        g[i] = a[j] & g[i-inter-1];
-                        g[i+inter] = b[j] & g[i-inter-1];
-                        ii = ii + 1; //go forward in the interval
-                        i = i + 1; // go  to next g
+                        g[i+ii] = a[j] & g[i+ii-inter-1];
+                        g[i+ii+inter] = b[j] & g[i+ii-inter-1];
                 end
-                i = i + inter + 1; //go to next g after the last one assigned
+                i = i + inter + ii; //go to next g after the last one assigned
                 j = j + 1; //go to next input bit
                 inter = inter + 2**j; // interval increases exponentially 
         end
+        //n = g[NNL:1]; //assign outputs
 end
+assign n = g[NNL:1];//assign outputs
 
-/*
-wire [NNL-1:0] g; //non-linear intermediate values
-
-genvar i,j, ii,inter;
-//integer i,j, ii,inter;
-generate
-//initial values
-i = 1; 
-j = 0; //index for g and index for a, b
-inter = 1; //interval
-assign g[0] = c;
-
-while (i < NNULL)
-begin 
-        assign g[i] = a[j] & b[j];
-        i = i + i;  //go to next g
-        ii = 0;
-        while (ii < inter)
-        begin
-                assign g[i] = a[j] & g[i-inter-1]
-                assign g[i+inter] = b[j] & g[i - inter-1]
-                ii = ii + 1; //go forward in the interval
-                i = i + 1; // go  to next g
-        end
-        i = i + inter + 1; //go to next g after the last one assigned
-        j = j + 1; //go to next input bit
-        inter = inter + 2**j; // interval increases exponentially 
-end
-endgenerate
-*/
-
-assign n = g[NNL:1];
 endmodule
