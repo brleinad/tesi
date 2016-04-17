43900d9079b6e3215c2453de91aa93f0bc06967c
brleinad <daniel.rodas.bautista@gmail.com>
2016-04-17 20:23:48 +0100

    set up synthesys for optimized decomposed adder

---
+++ cla_adder/opt_decomposed/syn/run.tcl
@@ -0,0 +1,18 @@
+set DESIGN_NAME "gen_cla_decomposed"
+#set DESIGN_NAME "gen_nonlinear_part"
+#set DESIGN_NAME "gen_linear_part"
+#set VARIANT_NAME "bit_generated"
+set VARIANT_NAME "16bit"
+
+#set RTL_SOURCE_FILES "../gen_nonlinear_part.v" 
+#set RTL_SOURCE_FILES "../gen_linear_part.v" 
+set RTL_SOURCE_FILES "../gen_linear_part.v ../gen_nonlinear_part.v ../gen_cla_decomposed.v"
+#set RTL_SOURCE_FILES " ../decomposed12bit_adder.v ../nonlinear_part12bit_adder.v ../linear_part12bit_adder.v"
+
+
+# call a script which sets up Design Compiler (same as writing the same commands in .synopsys_dc.setup file)
+source dc_setup.tcl
+
+# call a script which compiles the design
+source dc.tcl
+
